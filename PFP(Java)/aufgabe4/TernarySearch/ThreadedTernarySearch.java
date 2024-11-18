f0154f518026f92a715393b71c183e58dbe5c113
Metin Eren Heybet <100092820+merenheybet@users.noreply.github.com>
2024-11-18 23:48:05 +0100

    Created ThreadedTernarySearch

---
+++ PFP(Java)/aufgabe4/TernarySearch/ThreadedTernarySearch.java
@@ -0,0 +1,7 @@
+public class ThreadedTernarySearch implements ParallelTernarySearch{
+
+    @Override
+    public Double[] findMinimum(Function[] f, double[] left, double[] right, int nThreads, TernarySearch rootFinder) {
+        return new Double[0];
+    }
+}
