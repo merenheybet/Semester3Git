7134e09ca11494284fdb14d488a9496a3b97518d
Metin Eren Heybet <100092820+merenheybet@users.noreply.github.com>
2024-11-19 11:53:50 +0100

    I've really screwed up the project settings

--- PFP(Java)/aufgabe4/TernarySearch/ThreadedTernarySearch.java
+++ PFP(Java)/aufgabe4/TernarySearch/ThreadedTernarySearch.java
@@ -1,7 +1,26 @@
 public class ThreadedTernarySearch implements ParallelTernarySearch{
-
     @Override
     public Double[] findMinimum(Function[] f, double[] left, double[] right, int nThreads, TernarySearch rootFinder) {
-        return new Double[0];
+        Thread[] minThreads = new Thread[nThreads];
+        Double[] results = new Double[f.length];
+
+        for(int i = 0; i < f.length; i++){
+            final int finalI = i;
+            minThreads[i] = new Thread(){
+                @Override
+                public void run() {
+                    results[finalI] = rootFinder.findMinimum(f[finalI], left[finalI], right[finalI]);
+                }
+            };
+            minThreads[i].start();
+        }
+
+        for(int i = 0; i< f.length; i++){
+            try{
+                minThreads[i].join();
+            } catch (InterruptedException e) {
+                //
+            }
+        }
     }
 }
