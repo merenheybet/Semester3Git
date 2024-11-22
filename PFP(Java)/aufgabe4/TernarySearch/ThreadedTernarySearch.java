d794bab202542f8b91a1904ad1d02ff7d16c80f8
Metin Eren Heybet <100092820+merenheybet@users.noreply.github.com>
2024-11-22 21:42:50 +0100

    Implemented a multithreaded solution that doesn't work worse than the single-thread

--- PFP(Java)/aufgabe4/TernarySearch/ThreadedTernarySearch.java
+++ PFP(Java)/aufgabe4/TernarySearch/ThreadedTernarySearch.java
@@ -4,57 +4,52 @@ public class ThreadedTernarySearch implements ParallelTernarySearch {
         Thread[] minThreads = new Thread[nThreads];
         Double[] results = new Double[f.length];
 
-        final int iterationCount = Math.floorDiv(f.length, nThreads);
-        final int iterationCountRest = f.length - (iterationCount * nThreads);
+        final int elementPerThread = Math.ceilDiv(f.length, nThreads);
 
-        for (int i = 0; i < f.length; i++) {
-            results[i] = rootFinder.findMinimum(f[i], left[i], right[i]);
+        class TernaryThread extends Thread{
+            final Function[] f;
+            final double[] left;
+            final double[] right;
+            Double[] results;
+            final TernarySearch rootFinder;
+            final int elementPerThread;
+            final int threadNo;
+
+            TernaryThread(Function[] f, double[] left, double[] right,
+                          Double[] results, TernarySearch rootFinder,
+                          int elementPerThread, int threadNo){
+                this.f = f;
+                this.left = left;
+                this.right = right;
+                this.results = results;
+                this.rootFinder = rootFinder;
+                this.elementPerThread = elementPerThread;
+                this.threadNo = threadNo;
+            }
+
+            @Override
+            public void run(){
+                for(int i = 0; i < elementPerThread; i++){
+                    int currentIndex = (elementPerThread * threadNo) + i;
+                    if(currentIndex >= f.length){return;}
+                    results[currentIndex] =
+                            rootFinder.findMinimum(f[currentIndex], left[currentIndex], right[currentIndex]);
+                }
+            }
+        }
+
+
+        for(int i = 0; i < nThreads; i++){
+            minThreads[i] = new TernaryThread(f, left, right, results, rootFinder, elementPerThread, i);
+            minThreads[i].start();
+        }
+
+        for(int i = 0; i < nThreads; i++){
+            try{
+                minThreads[i].join();
+            } catch (InterruptedException e) {/**/}
         }
 
         return results;
     }
 }
-
-//        for (int j = 0; j < iterationCount; j++){
-//            for(int i = 0; i < nThreads; i++){
-//                final int index = (j * nThreads) + i;
-//                minThreads[i] = new Thread(){
-//                    @Override
-//                    public void run() {
-//                        results[index] = rootFinder.findMinimum(f[index], left[index], right[index]);
-//                    }
-//                };
-//                minThreads[i].start();
-//            }
-//
-//            for(int i = 0; i < nThreads; i++){
-//                try{
-//                    minThreads[i].join();
-//                } catch (InterruptedException e) {
-//
-//                }
-//            }
-//        }
-//
-//        for(int j = 0 ; j < iterationCountRest ; j++){
-//            final int index = (iterationCount * nThreads) + j;
-//            minThreads[j] = new Thread(){
-//                @Override
-//                public void run() {
-//                    results[index] = rootFinder.findMinimum(f[index], left[index], right[index]);
-//                }
-//            };
-//            minThreads[j].start();
-//        }
-//
-//        for(int j = 0 ; j < iterationCountRest ; j++){
-//            try{
-//                minThreads[j].join();
-//            } catch (InterruptedException e) {
-//
-//            }
-//        }
-//
-//        return results;
-//    }
-//}
