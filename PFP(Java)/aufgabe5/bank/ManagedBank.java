513d56f89f4a49d36940b7a9b13617be21a3038f
Metin Eren Heybet <100092820+merenheybet@users.noreply.github.com>
2024-12-02 12:56:21 +0100

    Added cashier and ManagedBank. They're definitely wrong but i don't want to keep doing this

--- PFP(Java)/aufgabe5/bank/ManagedBank.java
+++ PFP(Java)/aufgabe5/bank/ManagedBank.java
@@ -1,6 +1,39 @@
 public class ManagedBank implements Bank {
+
+    private static final Cashier cashier = new Cashier();
+
     @Override
+    // fast exakte Kopie von QuickLockingBank
     public boolean transfer(Account fromAccount, Account toAccount, int money) {
-        return false;
+        // Ask for permission!
+        if(!cashier.transactionAllowed(fromAccount, toAccount)){
+            return false;
+        }
+
+        // Lock fromAccount lock
+        try {
+            fromAccount.lock();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } try{
+            if(fromAccount.getMoney() < money){
+                //System.err.println("Not enough money");
+                return false;
+            }
+            // Lock toAccount
+            toAccount.lock();
+            try{
+                fromAccount.setMoney(fromAccount.getMoney() - money);
+                toAccount.setMoney(toAccount.getMoney() + money);
+                return true;
+            }finally{
+                toAccount.unlock();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally{
+            fromAccount.unlock();
+            cashier.completeTransaction(fromAccount, toAccount);
+        }
     }
 }
